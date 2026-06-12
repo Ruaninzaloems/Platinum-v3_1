f852e87aaaef14651b4ecd06885ce9657974857b
Ruan <Ruan.vdwesthuizen@inzaloems.co.za>
2026-06-12 23:24:40 +0200

    fix IDP backend connects

--- BUDGET-APP/PlatinumBudget.Api/obj/Debug/net10.0/PlatinumBudget.Api.AssemblyInfo.cs
+++ BUDGET-APP/PlatinumBudget.Api/obj/Debug/net10.0/PlatinumBudget.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatinumBudget.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e4171b88991cca22fa3e59b305767f7888c36f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e4608be5d74d90439bc9f0d56810ab20f62f957")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatinumBudget.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatinumBudget.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
